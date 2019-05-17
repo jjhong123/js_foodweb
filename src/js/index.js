88a7d6c876ec4ded9e4d4fccd6d79ed831431214
Yang <qaawssedd456@gmail.com>
2019-05-17 17:32:19 +0800

    Update

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,31 @@
-import Search from './Search'
+import Search from './models/Search'
 
-const search = new Search('pizza')
+/** Global state of the app
+ * - Search object
+ * - Current recioe object
+ * - Shopping list object
+ */
 
-// search.getResults()
+const state = {}
+
+const controlSearch = async () => {
+    // 1) Get query from view
+    const query = 'pizza' //T000
+
+    if (query) {
+        // 2) New search object and add to state
+        state.search = new Search(query)
+        // 3) Prepare UI for results
+
+        // 4) Search for recipes
+        await state.search.getResults()
+
+        // 5) Render results on UI
+        console.log(state.search.result)
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault()
+    controlSearch()
+})
